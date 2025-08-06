03ad1229770abf7b78b2ec646e8a0c31d6edbf4c
dip269 <janasudip2002@gmail.com>
2025-08-06 13:49:19 -0400

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+a =float(input("Enter your number:"))
+b=float(input("enter your number:"))
+print("addition=",a+b)
+print("subtraction=",a-b)
+print("multiplication=",a*b)
+print("division=",a/b)
---
+++ task2.py
@@ -0,0 +1,6 @@
+first=(input("enter your first name: "))
+second=(input("enter your second name: "))
+fullname=first+ " "+second
+print("Hello,",fullname,"! welcome to India.")
+
+
